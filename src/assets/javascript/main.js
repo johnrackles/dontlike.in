f89a48cd74657d9b54590e94a8c30df0aee29a92
John Rackles <john.rackles@gmail.com>
2016-04-18 20:22:06 +0200

    use airbnb javascript coding style

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,14 +1,10 @@
-function animateHeaderScroll() {
-  $(window).scroll(function () {
+$(document).ready(() => {
+  ($(window).scroll(() => {
     const $header = $('.header');
     if ($(window).scrollTop() > (72 - 49)) {
       $header.addClass('scrolled');
     } else {
       $header.removeClass('scrolled');
     }
-  });
-}
-
-$(document).ready(function () {
-  animateHeaderScroll();
+  })());
 });
