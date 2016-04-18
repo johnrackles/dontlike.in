5a272c14446ce0a1a85829d81350ec0772f1e8fc
John Rackles <john.rackles@gmail.com>
2016-04-18 20:23:38 +0200

    remove unnecessary self invoking function

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-  ($(window).scroll(() => {
+  $(window).scroll(() => {
     const $header = $('.header');
     if ($(window).scrollTop() > (72 - 49)) {
       $header.addClass('scrolled');
     } else {
       $header.removeClass('scrolled');
     }
-  })());
+  });
 });
